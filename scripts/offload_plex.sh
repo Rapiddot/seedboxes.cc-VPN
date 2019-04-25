5b96a1749b80c08d6c8a99627a62fccf32835cc0
amatsoukas <amatsoukas@rapiddot.com>
2019-04-25 14:54:41 +0300

    Script to automatically offload content to google drive

---
+++ scripts/offload_plex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# RClone Config file
+RCLONE_CONFIG=/home/user/.apps/plex/.config/rclone/rclone.conf
+export RCLONE_CONFIG
+LOCKFILE="/var/lock/`basename $0`"
+
+touch /home/user/.apps/plex/.config/rclone/upload_excludes
+
+(
+  # Wait for lock for 5 seconds
+  flock -x -w 5 200 || exit 1
+
+# Move older local files to the cloud
+/usr/bin/rclone move /home/user/files/Plex/ plex-gdrive:Plex/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs
+
+) 200> ${LOCKFILE}
