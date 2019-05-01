4d47ad206bfc06cf227ea2a3d6f7b19d932c094b
amatsoukas <amatsoukas@rapiddot.com>
2019-05-01 18:43:38 +0300

    Recreate the plex folder structure

--- scripts/offload_plex.sh
+++ scripts/offload_plex.sh
@@ -12,5 +12,5 @@ touch /home/user/.apps/plex/.config/rclone/upload_excludes
 
 # Move older local files to the cloud
 /usr/bin/rclone move /home/user/files/Plex/ plex-gdrive:Plex/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs
-
+mkdir -p /home/user/files/Plex/TV /home/user/files/Plex/Movies /home/user/files/Plex/Music
 ) 200> ${LOCKFILE}
