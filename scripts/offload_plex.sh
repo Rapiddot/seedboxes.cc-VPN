e31af26f4568e41e233cc81e5ac8aba88b63dd4d
amatsoukas <amatsoukas@rapiddot.com>
2019-05-16 19:23:17 +0300

    Include a custom user agent for rclone in order to circumvent google's api throttling

--- scripts/offload_plex.sh
+++ scripts/offload_plex.sh
@@ -11,6 +11,6 @@ touch /home/user/.apps/plex/.config/rclone/upload_excludes
   flock -x -w 5 200 || exit 1
 
 # Move older local files to the cloud
-/usr/bin/rclone move /home/user/files/Plex/ plex-gdrive:Plex/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs
+/usr/bin/rclone move /home/user/files/Plex/ plex-gdrive:Plex/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
 mkdir -p /home/user/files/Plex/TV /home/user/files/Plex/Movies /home/user/files/Plex/Music
 ) 200> ${LOCKFILE}
