358da12703b80e7622bc25a4a79c4669e117b977
amatsoukas <amatsoukas@rapiddot.com>
2019-06-16 09:22:01 +0300

    Set the folders independently

--- scripts/offload_plex.sh
+++ scripts/offload_plex.sh
@@ -11,6 +11,8 @@ touch /home/user/.apps/plex/.config/rclone/upload_excludes
   flock -x -w 5 200 || exit 1
 
 # Move older local files to the cloud
-/usr/bin/rclone move /home/user/files/Plex/ plex-gdrive:Plex/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-mkdir -p /home/user/files/Plex/TV /home/user/files/Plex/Movies /home/user/files/Plex/Music
+/usr/bin/rclone move /home/user/files/Plex/Movies/ plex-gdrive:Plex/Movies/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+/usr/bin/rclone move /home/user/files/Plex/TV/ plex-gdrive:Plex/TV/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+/usr/bin/rclone move /home/user/files/Plex/Music/ plex-gdrive:Plex/Music/ -P --checkers 3 --log-file /home/user/.apps/plex/.config/rclone/upload_rclone.log -v --tpslimit 3 --transfers 3 --drive-chunk-size 32M --exclude-from /home/user/.apps/plex/.config/rclone/upload_excludes --delete-empty-src-dirs --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+
 ) 200> ${LOCKFILE}
